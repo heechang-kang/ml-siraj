0ad689b3b72144fdc980bfcb6e22debff505d22e
heechang kang <heechang1997@gmail.com>
2019-09-16 02:03:14 +0900

    add conda env

--- w3_CyberSecurity/hello.py
+++ w3_CyberSecurity/hello.py
@@ -3,12 +3,15 @@ import numpy
 import json
 import sys
 import pandas
+import jsonify
 
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 
 import sklearn.externals import joblib
+
+import stripe
 
 app = Flask(__name__)
 
@@ -18,8 +21,24 @@ def test():
 
 filename = 'trained_model.sav'
 
+@app.route('signup'), methods=['GET']
+def signup_process():
+    card_data = request.json
+
+    charge = stripe
+
+
 @app.route('api/v0/test'), methods= ['GET', 'POST']
 def predict_test():
     request = pd.DataFrame([request.json])
     loaded_model = joblib.load(filename)
-    res = str(loaded_model.predict())
+    res = str(loaded_model.predict(vector)[0])
+
+    result = [
+        {
+            'id': 0
+            'prediction': res
+        }
+    ]
+
+    return jsonify(result)
